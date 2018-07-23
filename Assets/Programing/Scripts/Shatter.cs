a806f7a64d1d532a3e4327b0367f8ca834f8526e
Jacob Letko <s179061@students.aie.edu.au>
2018-07-23 14:22:13 -0700

    tmp

---
+++ Assets/Programing/Scripts/Shatter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// this goes on the object to be hit
+/// broken version is the same object but moddeld in pecies to break
+/// </summary>
+public class Shatter : MonoBehaviour
+{
+    public GameObject BrokenVersion;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject copy = Instantiate(BrokenVersion, transform.position, transform.rotation);
+            Destroy(gameObject);
+            Destroy(copy, 2);
+        }
+    }
+}
