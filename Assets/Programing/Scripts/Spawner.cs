1d27ea647cc3e9cc72c30a1f9f1446a19498cbc6
Jacob Letko <s179061@students.aie.edu.au>
2018-07-23 16:28:04 -0700

    stuff

--- Assets/Programing/Scripts/Spawner.cs
+++ Assets/Programing/Scripts/Spawner.cs
@@ -5,9 +5,26 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    bool destroy;
+    GameObject obj;
     public List<GameObject> objs = new List<GameObject>();
     public float timer;
+    Transform pos;
 
+    private void Start()
+    {
+        pos = transform;
+        Vector3 _pos = pos.position;
+        _pos.x = 0;
+        _pos.z = 0;
+        _pos.y = 2;
+        pos.position = _pos;
+    }
 
+    private void Update()
+    {
+        if(obj == null)
+        {
+            obj = Instantiate(objs[Random.Range(0, objs.Capacity)], pos);
+        }
+    }
 }
